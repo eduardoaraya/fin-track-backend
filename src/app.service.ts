e1aa3c583e9a474b7814be65fc546f61f5069b52
eduardoaraya <eduardo@eduardoaraya.araya>
2025-01-06 00:26:02 -0300

    feat(users): Set default structure to modules

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Ip } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInfo(ip: string): string {
+    return `Hello ${ip} | Application on server:`;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user';
+import { IUser } from './user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  async create(user: IUser): Promise<IUser> {
+    try {
+      return this.usersRepository.create(user);
+    } catch (error) {
+      throw new Error('Internal error on UserService');
+    }
+  }
+  async find(id: number): Promise<IUser | false> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: {
+          id,
+        },
+      });
+      return user ?? false;
+    } catch (error) {
+      throw new Error('Internal error on UserService');
+    }
+  }
+  async findAll(): Promise<IUser[]> {
+    try {
+      return this.usersRepository.find();
+    } catch (error) {
+      throw new Error('Internal error on UserService');
+    }
+  }
+}
